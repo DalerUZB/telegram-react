88d835fb14415bb3fa8d6b405f17d7e425f71322
DalerUZB <dalir2001mail.com@gmail.com>
2022-10-31 03:50:32 +0500

    login 50%

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -10,6 +10,7 @@ const Login = () => {
         <FormDiv>
           <input type="text" placeholder="username" />
           <input type="password" placeholder="password" />
+          <button>Отправитъ</button>
         </FormDiv>
       </div>
       <Image width={1550} height={900} />
@@ -33,18 +34,41 @@ const Wrapper = styled.div`
     width: 50%;
     min-height: 50%;
     border-radius: 8px;
+    transition: 0.3s all cubic-bezier(0.19, 1, 0.22, 1);
     padding: 20px;
-    text-align: center;
-
+    text-align: center; 
+    box-shadow: 0 0 220px 2px rgba(255,255,255,255);
+    
   }
 `;
 
 const FormDiv = styled.form`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
+  padding: 30px;
+  gap: 20px;
   input {
+    height: 49px;
+    max-width: 100px;
+    border-radius: 4px;
+    outline: 0;
+    border: none;
+    transition: 0.3s all;
+    padding-left: 4px;
+  }
+  button{
+   width: 100px;
+   border: none;
+   padding: 8px 16px;
+   border-radius: 4px;
+  }
+  input:focus {
+    height: 49px;
     max-width: 100%;
+    border-radius: 8px;
+    padding-left: 3px;
+    font-size: 15px;
+    background:rgba(0,0,0,0.4);
+    border: 1px solid white;
   }
 `;
