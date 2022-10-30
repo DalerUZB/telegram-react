bfebf348db2313476b6cad4178fb6d208cbc03c9
DalerUZB <DalerUZB>
2022-10-31 01:33:59 +0500

    login create-login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="*" element={<h1>not found</h1>} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/Pages/Create-Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateLogin = () => {
+  return <div>Create-Login</div>;
+};
+
+export default CreateLogin;
